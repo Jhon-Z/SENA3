f7f03b84fa0063b5cac453a8c92ecaf827a5c616
Jhon-Z <jezambrano20@misena.edu.co>
2019-06-26 11:46:36 -0500

    SENA3

--- main.js
+++ main.js
@@ -1,3 +1,37 @@
+var cadena1= "Carrito"
+
+// console.log(cadena1.length)
+
+
+// console.log(cadena1.charAt(0))
+// console.log(cadena1.charAt(1))
+// console.log(cadena1.charAt(2))
+// console.log(cadena1.charAt(3))
+// console.log(cadena1.charAt(4))
+
+// console.log(cadena1.toLowerCase())
+// console.log(cadena1.toUpperCase())
+
+var cadena1= "Carro BMW"
+// var cadena2= "Nuevo"
+
+// var cadenaNueva=cadena1.concat(cadena2)
+
+// console.log(cadena1.EndsWitch("p"))
+
+// console.log(cadena1.includes("rp"))
+
+// console.log(cadena1.indexOf("BMW"))
+
+// console.log(cadena1.slice(0,5))
+
+var cadena1="carro BMW"
+
+console.log(cadena1.substring(4,7))
+
+
+
+
 //alert("Hola, papuhs")
 //console.log ("Vamos a subir los archivos a git)
 // var persona= 
@@ -14,45 +48,47 @@
 // }
 
 
-var arrayPeople=[
-{nombre:"Padilla Shonnen",
-edad:18,
-cedula:"666",
-PeliFavorita:"Chucky",
-GeneroFavorito:"Terror"
-},
+// var arrayPeople=[
+// {nombre:"Padilla Shonnen",
+// edad:18,
+// cedula:"666",
+// PeliFavorita:"Chucky",
+// GeneroFavorito:"Terror"
+// },
 
-{nombre:"Samael",
-edad:15,
-cedula:"123456",
-PeliFavorita:"Chihiro",
-GeneroFavorito:"Anime"
-},
+// {nombre:"Samael",
+// edad:15,
+// cedula:"123456",
+// PeliFavorita:"Chihiro",
+// GeneroFavorito:"Anime"
+// },
 
-{nombre:"Jesse",
-edad:18,
-cedula:"456789",
-PeliFavorita:"Titanic",
-GeneroFavorito:"Drama"
-}
-]
+// {nombre:"Jesse",
+// edad:18,
+// cedula:"456789",
+// PeliFavorita:"Titanic",
+// GeneroFavorito:"Drama"
+// }
+// ]
 
-for (var i = 0; i <= arrayPeople.length; i++) {
+// for (var i = 0; i <= arrayPeople.length; i++) {
 
-alert(arrayPeople[i].nombre+"\n"+arrayPeople[i].edad+"\n"+arrayPeople[i].cedula+"\n"+arrayPeople[i].PeliFavorita+"\n"+arrayPeople[i].GeneroFavorito+"\n")
-if (arrayPeople[i].edad >=18 && arrayPeople[i].GeneroFavorito=="Terror") 
-{
- alert("Te recomendamos estos estrenos:")
- alert("Camino hacia el terror, El resplandor, La llamada")
-}
-else if (arrayPeople[i].GeneroFavorito=="Anime")
-{
- alert("Te recomendamos estos estrenos:")
- alert("Tenshi No Tamago, Nousicaa, Cuentos de Terramar")
-}
-else
-{
- alert("Te recomendamos estos estrenos:")
- alert("Hachico, Rocki Balboa, Rango")	
-}
-}
+// alert(arrayPeople[i].nombre+"\n"+arrayPeople[i].edad+"\n"+arrayPeople[i].cedula+"\n"+arrayPeople[i].PeliFavorita+"\n"+arrayPeople[i].GeneroFavorito+"\n")
+// if (arrayPeople[i].edad >=18 && arrayPeople[i].GeneroFavorito=="Terror") 
+// {
+//  alert("Te recomendamos estos estrenos:")
+//  alert("Camino hacia el terror, El resplandor, La llamada")
+// }
+// else if (arrayPeople[i].GeneroFavorito=="Anime")
+// {
+//  alert("Te recomendamos estos estrenos:")
+//  alert("Tenshi No Tamago, Nousicaa, Cuentos de Terramar")
+// }
+// else
+// {
+//  alert("Te recomendamos estos estrenos:")
+//  alert("Hachico, Rocki Balboa, Rango")	
+// }
+// }
+
+
